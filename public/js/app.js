2d59f9249effef9289b2d068c6ffd7fe7cf34425
DorianA <dorian9armijos@gmail.com>
2022-09-02 13:10:54 -0500

    feat:Se agrego el import de las funciones globales

--- public/js/app.js
+++ public/js/app.js
@@ -3,5 +3,13 @@
  * principal para correr el funcionamiento en las diferentes vistas 
  * del navegador
  */
-
+/**
+ * Este import importa el controlador principal para el uso de los 
+ * scripts de las diferentes vistas
+ */
 import './../../src/frontend/src/controller/mainController.js';
+/**
+ * Este import importa las funciones que se aplican a las diferentes vistas 
+ * pero de forma global
+ */
+import './../../src/frontend/src/utiles/funciones.js';
