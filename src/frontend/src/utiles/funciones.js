f9faa1a0770f180986261933c9d5bd5a9892cb4d
DorianA <dorian9armijos@gmail.com>
2022-09-04 16:19:52 -0500

    feat: Funcionamiento del ojo

--- src/frontend/src/utiles/funciones.js
+++ src/frontend/src/utiles/funciones.js
@@ -10,11 +10,39 @@
     const content = new URL(location.href);
     const items = menuPrincipal.querySelectorAll('a');
     items.forEach( i => {
-        const ref = i.href.trim();
-        const contentInterno = content.href.trim();
-        if(ref === contentInterno){
-            i.classList.add('is-activo-item-menu');
+        const refItem = new URL(i.href).pathname.trim(); // Contiene el url del path de cada <a> del menu principal
+        const dirItem = content.pathname.trim();// Contiene el path del navegador web como '/admin' 
+
+        if(refItem === dirItem){ // Si los dos path son iguales se le agrega la clase de que esa opcion esta activa actualmente
+            i.classList.add('is-activo-item-menu'); // por ejemplo en el navegador esta el path /admin/agregar/coordinador
+            // y en el <a href="/admin/agregar/coordinador"> es igual a /admin/agregar/coordinador se marca con la clase activa
         }
     })
 
+})();
+
+/**
+ * Esta funcion perimite mostrar las contrasenias 
+ */
+ (function(){
+    const ojo = document.querySelectorAll('#eye'); 
+    if(!ojo && ojo.length === 0) return;
+    ojo.forEach( e => {
+        e.addEventListener('click',convert);
+    })
+
+    function convert(e){
+        e.preventDefault();
+        let input = e.target.previousElementSibling;
+        if(input.hasAttribute('text')){
+            input.removeAttribute('text');
+            input.type = 'password';
+            e.target.parentNode.classList.add('raya');
+        }else{
+            input.setAttribute('text','');
+            input.type = 'text';
+            e.target.parentNode.classList.remove('raya');
+        }
+        
+    }
 })();
