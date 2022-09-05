fb4849cecbfecd1fa1ade237b8fc104d84af1010
DorianA <dorian9armijos@gmail.com>
2022-09-05 12:32:48 -0500

    feat: Agrego estilos de tablas

--- src/backend/Views/administrador/agregar_periodo_academico.html.php
+++ src/backend/Views/administrador/agregar_periodo_academico.html.php
@@ -1,56 +1,60 @@
 <div class="centrado-linea">
-<table class="tabla tabla-vertical contenedor-x100 margin-arriba-2 centrado-linea">
-            <thead>
-                <tr class="titulo-tablas">
-                    <th colspan="4">
-                        <div>
-                        <span class="material-icons">&#xe241;</span>
-                        <strong>Lista de Periodos Académicos</strong>
-                        </div>
-                    </th>
-                </tr>
-                <tr>
-                    <th>Periodo Académico</th>
-                    <th>Fecha de Incio</th>
-                    <th>Fecha de Finalización</th>
-                    <th> Opción </th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr class="p-relativo">
-                    <td>
-                        <div class="flex-linea flex-items-center input-personalizado-login">
-                            <span class="material-icons">&#xe8b5;</span>
-                            <input type="text" name="periodo" id="ciclo-academico" placeholder="Periodo Académico">
-                        </div>
-                        <p class="mensaje-error mensaje-error-flotante p-absoluta oculto">
-                            Error la entrada no es valida
-                        </p>
-                    </td>
-                    <td>
-                        <div class="flex-linea flex-items-center input-personalizado-login">
-                            <label for="f_i"><span class="material-icons">&#xebcc;</span></label> 
-                            <input type="date" id="f_i" name="fecha_inicial">
-                        </div>
-                    </td>
-                    <td>
-                        <div class="flex-linea flex-items-center input-personalizado-login">
-                           <label for="f_f"><span class="material-icons">&#xe916;</span></label> 
-                            <input type="date" id="f_f" name="fecha_final">
-                        </div>
-                    </td>
-                    <td>
-                        <button class="boton boton-enviar is-hover-boton-enviar block centrado-linea boton-iconos">
-                        <span class="material-icons text-blanco">&#xe3c9;</span>
-                        Editar 
-                        </button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-
-    <form class="margin-arriba-3">
-        <table class="tabla tabla-vertical contenedor-x100 margin-arriba-2 centrado-linea">
+<div class="altura-2 desborde-auto barra-personalizada padding-all-1 ">
+    <table class="tabla tabla-vertical contenedor-x100 sombra centrado-linea " id="tabla-presentacion">
+                <thead>
+                    <tr class="titulo-tablas">
+                        <th colspan="4">
+                            <div>
+                            <span class="material-icons">&#xe241;</span>
+                            <strong>Lista de Periodos Académicos</strong>
+                            </div>
+                        </th>
+                    </tr>
+                    <tr>
+                        <th>Periodo Académico</th>
+                        <th>Fecha de Incio</th>
+                        <th>Fecha de Finalización</th>
+                        <th> Opción </th>
+                    </tr>
+                </thead>
+                <tbody>
+                   <tr>
+                    <td class="is-cargando-contenido"></td>
+                    <td class="is-cargando-contenido"></td>
+                    <td class="is-cargando-contenido"></td>
+                    <td class="is-cargando-contenido"></td>
+                   </tr>
+                   <tr>
+                    <td class="is-cargando-contenido"></td>
+                    <td class="is-cargando-contenido"></td>
+                    <td class="is-cargando-contenido"></td>
+                    <td class="is-cargando-contenido"></td>
+                   </tr>
+                   <tr>
+                    <td class="is-cargando-contenido"></td>
+                    <td class="is-cargando-contenido"></td>
+                    <td class="is-cargando-contenido"></td>
+                    <td class="is-cargando-contenido"></td>
+                   </tr>
+                   <tr>
+                    <td class="is-cargando-contenido"></td>
+                    <td class="is-cargando-contenido"></td>
+                    <td class="is-cargando-contenido"></td>
+                    <td class="is-cargando-contenido"></td>
+                   </tr>
+                   <tr>
+                    <td class="is-cargando-contenido"></td>
+                    <td class="is-cargando-contenido"></td>
+                    <td class="is-cargando-contenido"></td>
+                    <td class="is-cargando-contenido"></td>
+                   </tr>
+                   
+                </tbody>
+            </table>
+</div>
+<div class="desborde-auto barra-personalizada padding-all-1 ">
+    <form class="">
+        <table class="tabla tabla-vertical contenedor-x100 margin-arriba-2 centrado-linea sombra">
             <thead>
                 <tr class="titulo-tablas">
                     <th colspan="4">
@@ -97,4 +101,5 @@
             </tbody>
         </table>
     </form>
+    </div>
 </div>
