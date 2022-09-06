71fb5fafc8aa7978e65cfcb9c73d7f5c7a2c4cb1
DorianA <dorian9armijos@gmail.com>
2022-09-06 10:40:31 -0500

    feat: Aumento de funcionamiento listar periodos

--- src/frontend/src/pages/admin/cicloAcademico.js
+++ src/frontend/src/pages/admin/cicloAcademico.js
@@ -9,6 +9,9 @@ const formulario = document.forms[0];
 const tablaPresentacion = document.getElementById('tabla-presentacion');
 let errores = true;
 let precarga = undefined;
+let finalizar = false; // esta variable funciona como bandera para cuando llamen a un renderPeriodosAcedemicos
+// cuando ingresan un nuevo periodo se renderize lo recien ingresado por medio de la peticion al servidor pero 
+// que el usuario vea que esta cargando los periodos academicos 
 function verdatos(e){
     e.preventDefault();
 }
@@ -53,7 +56,10 @@ async function accionEnviar(){
 
 function render(datos){
     if(datos.result){
-        precarga.end();
+        finalizar = true; // Aqui cambiamos la variable a true para que tenga acceso la funcion de rendirazdo de los periodos
+        // academicos a finalizar la precarga despues de que esta haya rendirazados todos los ciclos academicos
+        cargarPeriodosAcademicos();
+
         new Notificacion(
             'Se guardo con exito el nuevo periodo academico',
             'Aceptar',
@@ -69,6 +75,7 @@ function render(datos){
         )
     }
 
+
 }
 
 function manejarErrores(e){
@@ -79,9 +86,6 @@ function manejarErrores(e){
 }
 
 async function cargarPeriodosAcademicos(){
-    //precarga = new Precarga();
-    //precarga.run();
-    // console.log(e)
     PeriodoAcademico.getDatos()
     .then(renderPeriodosAcademicos)
     .catch(manejarErrores)
@@ -89,28 +93,39 @@ async function cargarPeriodosAcademicos(){
 
 cargarPeriodosAcademicos();
 
-function renderPeriodosAcademicos({periodoAcademico} = datos){
-    let html = '';
-    const tbody = tablaPresentacion.querySelector('tbody');
-    periodoAcademico.forEach(periodo => {
-        html += `<tr>
-        <td>
-           ${periodo.id}
-        </td>
-        <td>
-           ${periodo.fecha_inicial}
-        </td>
-        <td>
-            ${periodo.fecha_final}
-        </td>
-        <td>
-            <button class="boton boton-enviar is-hover-boton-enviar block centrado-linea boton-iconos">
-            <span class="material-icons text-blanco">&#xe3c9;</span>
-            Editar 
-            </button>
-        </td>
-    </tr>`;
-    });
-    tbody.innerHTML = html;
+function renderPeriodosAcademicos(respuesta){
+    if(respuesta.ident){
+        const {periodoAcademico} = respuesta;
+        let html = '';
+        const tbody = tablaPresentacion.querySelector('tbody');
+        periodoAcademico.forEach(periodo => {
+            html += `<tr>
+            <td>
+            ${periodo.id}
+            </td>
+            <td>
+            ${periodo.fecha_inicial}
+            </td>
+            <td>
+                ${periodo.fecha_final}
+            </td>
+            <td>
+                <button class="boton boton-enviar is-hover-boton-enviar block centrado-linea boton-iconos">
+                <span class="material-icons text-blanco">&#xe3c9;</span>
+                Editar 
+                </button>
+            </td>
+        </tr>`;
+        });
+        tbody.innerHTML = html;
+        if(finalizar) precarga.end(); // la variable finalizar la primera vez se va encontrar en falso lo cual no ejcutara
+        // terminar la precarga ya que si lo hace saltara un erro por que un primer instante la variable es undefined
+        // en instancias posteriores se encuentra con la clase de la precarga 
+    }else{
+        throw new Error(
+            'No se pudo cargar la lista de periodos académicos, '+ respuesta.mensaje
+            );
+    }
+    
 }
 
