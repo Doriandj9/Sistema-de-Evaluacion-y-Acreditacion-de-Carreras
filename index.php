25eeb1f9891953c42e2dc18409531ac411f9e9f4
DorianA <dorian9armijos@gmail.com>
2022-09-06 11:56:14 -0500

    fix: Cambio de ENV a config/database.php

--- index.php
+++ index.php
@@ -3,13 +3,11 @@ include __DIR__ . '/vendor/autoload.php';
 
 use App\backend\Application\ContentRoutes;
 use App\backend\Frame\EntryPoint;
-use Symfony\Component\Dotenv\Dotenv;
 
 
 
 $route = rtrim(ltrim(strtok($_SERVER['REQUEST_URI'],'?'),'/'),'/');
-$dotenv = new Dotenv();
-$dotenv->load(__DIR__ . '/config/.env');
+
 try{
 
     $entryPoint = new EntryPoint($route,$_SERVER['REQUEST_METHOD'],new ContentRoutes);
