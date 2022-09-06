caa65739c1387efab883cedb3f37bb4158c41370
DorianA <dorian9armijos@gmail.com>
2022-09-06 11:57:00 -0500

    feat: Se agrego la coneccion a la base de datos

--- config/database.php
+++ config/database.php
@@ -1,2 +1,32 @@
 <?php
 require __DIR__ . './../vendor/autoload.php';
+
+use Symfony\Component\Dotenv\Dotenv;
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__ . '/config/.env');
+ 
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver' => 'pgsql',
+    'host' => 'localhost',
+    'database' => 'sac_2022',
+    'username' => 'postgres',
+    'password' => 'barcelona',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+]);
+
+// Set the event dispatcher used by Eloquent models... (optional)
+use Illuminate\Events\Dispatcher;
+use Illuminate\Container\Container;
+$capsule->setEventDispatcher(new Dispatcher(new Container));
+
+// Make this Capsule instance available globally via static methods... (optional)
+$capsule->setAsGlobal();
+
+// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+$capsule->bootEloquent();
