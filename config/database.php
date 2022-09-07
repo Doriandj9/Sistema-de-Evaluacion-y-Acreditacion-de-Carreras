2355d48c77cd995fbc993f898cf58319f0395f81
DorianA <dorian9armijos@gmail.com>
2022-09-07 09:59:13 -0500

    feat: Configuracion de la base de datos ->Eloquent

--- config/database.php
+++ config/database.php
@@ -2,24 +2,24 @@
 require __DIR__ . './../vendor/autoload.php';
 
 use Symfony\Component\Dotenv\Dotenv;
-use Illuminate\Events\Dispatcher;
-use Illuminate\Container\Container;
 use Illuminate\Database\Capsule\Manager as Capsule;
 
 $dotenv = new Dotenv();
 $dotenv->load(__DIR__ . '/.env');
 
 $capsule = new Capsule;
-
 $capsule->addConnection([
-    'driver' => 'pgsql',
-    'host' => 'localhost',
-    'database' => 'seac_2022',
-    'username' => 'postgres',
-    'password' => 'barcelona',
+    'driver' => $_ENV['DRIVER'],
+    'host' => $_ENV['HOST'],
+    'database' => $_ENV['DBNAME'],
+    'username' => $_ENV['DBUSER'],
+    'password' => $_ENV['PASSDATABASE'],
     'charset' => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix' => '',
 ]);
 $capsule->setAsGlobal();
 $capsule->bootEloquent();
+    
+
+    
