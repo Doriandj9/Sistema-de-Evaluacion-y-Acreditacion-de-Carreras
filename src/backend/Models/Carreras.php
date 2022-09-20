caabf8033c4a979053cca0c6d3c29dfca1b215b6
DorianA <dorian9armijos@gmail.com>
2022-09-20 18:27:45 -0500

    feat:Query unico clase obtener docentes carreras

--- src/backend/Models/Carreras.php
+++ src/backend/Models/Carreras.php
@@ -5,11 +5,15 @@ declare(strict_types=1);
 namespace App\backend\Models;
 
 use App\backend\Application\Utilidades\DB;
-use Illuminate\Database\Eloquent\Model;
 
-class Carreras extends Model
+class Carreras extends DatabaseTable
 {
     public const TABLE = 'carreras';
+
+    public function __construct()
+    {
+        parent::__construct(self::TABLE, 'id');
+    }
 
     public function getDatosDocentes(string $idCarrera): \Illuminate\Support\Collection|bool
     {
@@ -17,8 +21,8 @@ class Carreras extends Model
         ->find(trim($idCarrera));
         if ($carrera) {
             $docentes_carreras = DB::table(self::TABLE)
-            ->join('docentes_carreras','carreras.id','=','docentes_carreras.id_carreras')
-            ->join('docentes','docentes.id','=','docentes_carreras.id_docentes')
+            ->join('docentes_carreras', 'carreras.id', '=', 'docentes_carreras.id_carreras')
+            ->join('docentes', 'docentes.id', '=', 'docentes_carreras.id_docentes')
             ->get();
             return $docentes_carreras;
         } else {
