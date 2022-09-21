1926df9c2a262466b5e37866b1e2861033c2f1ac
DorianA <dorian9armijos@gmail.com>
2022-09-20 19:15:02 -0500

    test: Inclusion de database.php de Eloquent

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,12 +5,11 @@ declare(strict_types=1);
 namespace Tests;
 
 use PHPUnit\Framework\TestCase as FrameworkTestCase;
-use Symfony\Component\Dotenv\Dotenv;
 /**
  * Esta clase extiende de TestCase de PhpUnit y realiza la accion
  * de inicializar las variables globales guardadas en el archivo
  * .env para uso de la base de datos
- * 
+ *
  * @author Dorian Armijos
  * @link <>
  * @author Nataly Fernandes
@@ -21,7 +20,6 @@ class TestCase extends FrameworkTestCase
     public function __construct()
     {
         parent::__construct();
-        $dotenv = new Dotenv();
-        $dotenv->load(__DIR__ . '/../config/.env');
+        include __DIR__ . '/../config/database.php';
     }
 }
