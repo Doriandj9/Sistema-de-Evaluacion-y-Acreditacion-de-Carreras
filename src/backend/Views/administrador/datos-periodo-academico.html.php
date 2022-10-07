ec015f326448cb4f003109c05da98363504ae273
DorianA <dorian9armijos@gmail.com>
2022-10-07 14:43:31 -0500

    delete: Mal ubicacion dentro de las carpetas

--- src/backend/Views/administrador/datos-periodo-academico.html.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo json_encode(
-    [
-        'periodoAcademico' => $periodoAcademico,
-        'ident' => 1
-    ],
-    JSON_UNESCAPED_UNICODE
-);
--- src/backend/Views/docentes/datos.html.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo json_encode(
-    [
-        'docentes' => $docentes,
-        'ident' => 1
-    ],
-    JSON_UNESCAPED_UNICODE
-);
