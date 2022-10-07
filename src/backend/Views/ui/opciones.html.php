49d572626326abda93d51d35460a0dab77ed219b
DorianA <dorian9armijos@gmail.com>
2022-10-07 15:02:06 -0500

    build: vista de opciones

---
+++ src/backend/Views/ui/opciones.html.php
@@ -0,0 +1,63 @@
+<div class="d-flex w-100 h-100 justify-content-between mt-4">
+  <div class="w-25  dropend ms-3" id="menu-opciones">
+        <div class="container-fluid bg-primary p-2 text-white text-center">
+            Menu
+        </div>
+        <button data-content-permision="2" type="button" class="btn btn-color-gris container-fluid color-active rounded-0
+        d-flex align-items-center gap-2 text-start " data-bs-toggle="dropdown" aria-expanded="false">
+        <span class="material-icons">&#xe5c8;</span> <span class="fw-bold">Secretaría</span>  
+        </button>
+        <ul class="dropdown-menu w-100 d-none">
+        </ul>
+
+        <button data-content-permision="4" type="button" class="btn btn-color-gris container-fluid color-active rounded-0
+        d-flex align-items-center gap-2 text-start " data-bs-toggle="dropdown" aria-expanded="false">
+        <span class="material-icons">&#xe5c8;</span> <span class="fw-bold">Coordinador</span>  
+        </button>
+        <ul class="dropdown-menu w-100 d-none">
+        </ul>
+
+        <button data-content-permision="1" type="button" class="btn btn-color-gris container-fluid color-active rounded-0 
+        d-flex align-items-center gap-2 text-start " data-bs-toggle="dropdown" aria-expanded="false">
+        <span class="material-icons">&#xe5c8;</span> <span class="fw-bold">Docente</span>  
+        </button>
+        <ul class="dropdown-menu w-100 d-none">
+        </ul>
+
+        <button data-content-permision="8" type="button" class="btn btn-color-gris container-fluid color-active rounded-0
+        d-flex align-items-center gap-2 text-start " data-bs-toggle="dropdown" aria-expanded="false">
+        <span class="material-icons">&#xe5c8;</span> <span class="fw-bold">Evaluador</span>  
+        </button>
+        <ul class="dropdown-menu w-100 d-none">
+        </ul>
+        <a href="/salir" class="btn btn-color-gris text-danger container-fluid color-active rounded-0
+        d-flex align-items-center gap-2 text-start">
+        <span class="material-icons">&#xe5c8;</span> <span class="fw-bold">Salir</span>  
+        </a>
+        <ul class="dropdown-menu w-100 d-none">
+        </ul>
+  </div>
+  <div class="w-65 h-90 bg-secondary me-3 d-flex justify-content-center align-items-end">
+      <div class="cuadro-blaco-p mb-2 d-flex flex-column">
+      <div class="w-100 h-p-1 bg-color-gris d-flex justify-content-around  align-items-end">
+        <div class="position-relative ancho1">  
+          <div class="cuadro-redondo">
+            <img src="/public/assets/img/undraw_book_lover_re_rwjy.svg" class="position-absolute img-header-options" height="54" width="64"  alt="">
+          </div>
+        </div>
+        <div class="d-flex justify-content-between flex-grow-1">
+          <div class="fw-bold">Carreras Acreditadas</div>
+          <div class="d-flex w-50 justify-content-evenly fw-bold">
+            <div class="d-flex align-items-center gap-2"> <span class="material-icons">&#xe8df;</span><span id="fecha"></span></div>
+            <div class="d-flex align-items-center gap-2"> <span class="material-icons">&#xe924;</span><span id="hora"></span></div>
+          </div>
+        </div>
+      </div>
+        <div class="flex-grow-1 d-flex justify-content-center align-items-center" id="contenido">
+              
+              
+              
+        </div>
+      </div>
+  </div>
+</div>
