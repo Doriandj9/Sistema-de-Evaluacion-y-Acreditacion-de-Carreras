c30775da806d0072fd15a102dfcd1c6a2462328b
DorianA <dorian9armijos@gmail.com>
2022-10-07 14:36:04 -0500

    feat: A la sesion se agrego el permiso

--- src/backend/Controllers/Administrador/Inicio.php
+++ src/backend/Controllers/Administrador/Inicio.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace App\backend\Controllers\Administrador;
 
 use App\backend\Controllers\Controller;
+use App\backend\Models\Docente;
 
 class Inicio implements Controller
 {
     public function vista($variables = []): array
     {
+        $_SESSION['permiso'] = Docente::ADMIN;
         return [
             'title' => 'Inicio Administrador',
             'template' => 'administrador/inicio.html.php'
