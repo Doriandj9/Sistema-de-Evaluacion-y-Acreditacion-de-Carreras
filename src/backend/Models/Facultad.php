c23db29828b915d28fd25372e7547dce7f92e9c9
DorianA <dorian9armijos@gmail.com>
2022-10-14 19:47:00 -0500

    build: Modelo de Facultad

---
+++ src/backend/Models/Facultad.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\backend\Models;
+
+class Facultad extends DatabaseTable
+{
+    public const TABLE = 'facultad';
+
+    public function __construct()
+    {
+        parent::__construct(self::TABLE,'id');
+    }
+}
