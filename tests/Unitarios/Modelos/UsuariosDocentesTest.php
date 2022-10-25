986a4e506badfbd8595fc6e7f291c7f405be63e7
DorianA <dorian9armijos@gmail.com>
2022-10-25 11:14:19 -0500

    build: test para los usuarios docentes como actualizar las fechas de un coordinador

---
+++ tests/Unitarios/Modelos/UsuariosDocentesTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Unitarios\Modelos;
+
+use App\backend\Models\Docente;
+use App\backend\Models\UsuariosDocente;
+use Tests\TestCase;
+
+class UsuariosDocentesTest extends TestCase
+{
+    private UsuariosDocente $usuariosDocentes;
+    private array $datosActualizar;
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    protected function setUp(): void
+    {
+        $this->usuariosDocentes = new UsuariosDocente;
+        $this->datosActualizar = [
+            'fecha_inicial' => '2022-10-03',
+            'fecha_final' => '2052-10-03',
+            'estado' => 'activo'
+        ];
+    }
+    /**
+     * @covers \App\backend\Models\UsuariosDocente::updateUsuario
+     */
+    public function testActualizarFechasDeUnCoordinador() {
+        $id_usuario = Docente::COORDINADORES;
+        $id_docente = '0250186664';
+        $result = $this->usuariosDocentes->updateUsuario($id_usuario,$id_docente,$this->datosActualizar);
+
+        $this->assertTrue($result);
+    }
+
+    public function testObtenerUsuriosCoordinadores() {
+        $coordinadores = $this->usuariosDocentes->obtenerCoordinadores();
+        var_dump($coordinadores);
+    }   
+}
