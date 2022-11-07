f23b333b39eac31f405af271df3db4f435bbfafb
DorianA <dorian9armijos@gmail.com>
2022-11-06 19:58:59 -0500

    deploy errors

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 include __DIR__ . '/vendor/autoload.php';
 include __DIR__ . '/config/database.php';
 
@@ -6,6 +9,7 @@ use App\backend\Application\ContentRoutes;
 use App\backend\Frame\EntryPoint;
 
 $route = rtrim(ltrim(strtok($_SERVER['REQUEST_URI'],'?'),'/'),'/');
+
 try{
 
     $entryPoint = new EntryPoint($route,$_SERVER['REQUEST_METHOD'],new ContentRoutes);
