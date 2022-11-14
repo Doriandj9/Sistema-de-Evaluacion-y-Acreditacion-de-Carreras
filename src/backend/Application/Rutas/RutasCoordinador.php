008d07f77c6f341c44b9147ad303b9279afc4bf7
DorianA <dorian9armijos@gmail.com>
2022-11-13 23:27:43 -0500

    fea: rutas para docente y Evidencias

--- src/backend/Application/Rutas/RutasCoordinador.php
+++ src/backend/Application/Rutas/RutasCoordinador.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\backend\Application\Rutas;
 
+use App\backend\Controllers\Coordinador\Docentes;
+use App\backend\Controllers\Coordinador\Evidencias;
 use App\backend\Controllers\Coordinador\Inicio;
 use App\backend\Frame\Route;
 use App\backend\Models\Docente;
@@ -13,13 +15,39 @@ class RutasCoordinador implements Route
     public function getRoutes(): array
     {
         $inicioController = new Inicio;
+        $docentes = new Docentes;
+        $evidencias = new Evidencias;
         return [
             'coordinador' => [
                 'GET' => [
                     'controller' => $inicioController,
                     'action' => 'vista'
                 ]
-            ]
+                ],
+            'coordinador/docentes' => [
+                'GET' => [
+                    'controller' => $docentes,
+                    'action' => 'vista'
+                ]
+                ],
+            'coordinador/datos/docentes/carrera' => [
+                'GET' => [
+                    'controller' => $docentes,
+                    'action' => 'listarDocentes'
+                ],
+            ],
+            'coordinador/evidencias' => [
+                'GET' => [
+                    'controller' => $evidencias,
+                    'action' => 'vista'
+                ],
+            ],
+            'coordinador/registrar/docente' => [
+                'POST' => [
+                    'controller' => $docentes,
+                    'action' => 'registar'
+                ],
+            ],
         ];
     }
     public function getTemplate(): string
