5161d72fd24b938ecc827f75c4e5c78b8062491b
DorianA <dorian9armijos@gmail.com>
2022-11-13 23:32:56 -0500

    buid: vista de evidencias frontend

---
+++ src/backend/Views/coordinadores/evidencias.html.php
@@ -0,0 +1,103 @@
+<div class="w-100" style="margin-top: 5rem;">
+                           
+                        <div class="contenedor-items-menu-superior tipografia-times-2" id="contenedor-menu-superior">
+                            <ul>
+                                <li class="is-activo-op-menu">Listar Evidencias</li>
+                                <li>Registrar Evidencias</li>
+                            </ul>
+                        </div>
+                        <div class="w100" id="cambio-vistas">
+                          <div class="w-100 h-100 d-flex justify-content-center align-items-center">
+                            <div class="spinner-border text-primary" role="status">
+                              <span class="visually-hidden">Loading...</span>
+                            </div>
+                          </div>
+                        </div>
+
+</div>
+<!-- Vistas  -->
+<template id="template-listar-evidencias">
+<div class="w-100 d-flex justify-content-center">
+<div class="desborde-auto barra-personalizada padding-all-1 sombra w-50 mb-4">
+<div class="w-100 d-flex justify-content-center mb-3">
+    <div class="contenedor-filtro ps-2 pe-2">
+        <span class="material-icons text-white">&#xe152;</span>
+        <span class="text-white">Periodo </span>
+    </div>
+    <div class="contenedor-busqueda w-75" id="content-busqueda">
+        <select class="w-100" name="" id="">
+            <option value="none">cargando...</option>
+        </select>
+    </div>
+</div>
+<div class="w-100 d-flex justify-content-center">
+    <div class="contenedor-filtro ps-2 pe-2">
+        <span class="material-icons text-white">&#xe152;</span>
+        <span class="text-white">Evidencia </span>
+    </div>
+    <div class="contenedor-busqueda w-75" id="content-busqueda">
+        <span class="material-icons">&#xe8b6;</span>
+        <input type="text" name="valor" id="busqueda" placeholder="Busque por el nombre.">
+    </div>
+</div>
+</div>
+</div>
+<div class="w-100">
+<table class="table table-striped-columns w-100">
+    <thead>
+      <tr>
+        <th class="bg-primary text-white text-center">Criterio</th>
+        <th class="bg-primary text-white text-center">Estandar Indicador</th>
+        <th class="bg-primary text-white text-center">Elemento Fundamental</th>
+        <th class="bg-primary text-white text-center">Componente</th>
+        <th class="bg-primary text-white text-center">Documento de Información</th>
+        <th class="bg-primary text-white text-center">Fecha de Habilitación </th>
+        <th class="bg-primary text-white text-center">Finalización de Entrega </th>
+        <th class="bg-primary text-white text-center">Opción</th>
+      </tr>
+    </thead>
+    <tbody>
+                    <tr>
+                    <td class="is-cargando-contenido p-5"></td>
+                    <td class="is-cargando-contenido p-5"></td>
+                    <td class="is-cargando-contenido p-5"></td>
+                    <td class="is-cargando-contenido p-5"></td>
+                    <td class="is-cargando-contenido p-5"></td>
+                    <td class="is-cargando-contenido p-5"></td>
+                    <td class="is-cargando-contenido p-5"></td>
+                    <td class="is-cargando-contenido p-5"></td>
+                   </tr>
+                   <tr>
+                    <td class="is-cargando-contenido p-5"></td>
+                    <td class="is-cargando-contenido p-5"></td>
+                    <td class="is-cargando-contenido p-5"></td>
+                    <td class="is-cargando-contenido p-5"></td>
+                    <td class="is-cargando-contenido p-5"></td>
+                    <td class="is-cargando-contenido p-5"></td>
+                    <td class="is-cargando-contenido p-5"></td>
+                    <td class="is-cargando-contenido p-5"></td>
+                   </tr>
+                   <tr>
+                    <td class="is-cargando-contenido p-5"></td>
+                    <td class="is-cargando-contenido p-5"></td>
+                    <td class="is-cargando-contenido p-5"></td>
+                    <td class="is-cargando-contenido p-5"></td>
+                    <td class="is-cargando-contenido p-5"></td>
+                    <td class="is-cargando-contenido p-5"></td>
+                    <td class="is-cargando-contenido p-5"></td>
+                    <td class="is-cargando-contenido p-5"></td>
+                   </tr>
+    </tbody>
+  </table>
+  <div class="contenedor-numeros-paginacion d-flex justify-content-center"></div>
+</div>
+</template>
+
+<!-- TODO: Vista de registrar docentes -->
+<template id="template-registar-evidencias">
+<div class="w-75">
+<form class="tipografia-times-2" id="form-insert">
+   
+  </form>
+</div>
+</template>
