4639fd50db863bbb0189292c952daebaad7d4485
DorianA <dorian9armijos@gmail.com>
2022-11-21 13:54:37 -0500

    build: archivos que controlan los script y archivos de scrips para las evidencias  y reportes

---
+++ src/frontend/src/controller/docenteController.js
@@ -0,0 +1,30 @@
+(function(){
+    const url = new URL(location.href);
+    let path = url.pathname;
+    let arrayPath = path.split('/')
+    if(arrayPath[arrayPath.length-1] === '') 
+        path = path.substring(0,path.length-1);
+    const controllerMain = {
+        "/docente/reportes": function(){
+            importacionDinamica('reportes.js');
+        },
+        "/docente/evidencias": function(){
+            importacionDinamica('evidencias.js');
+        },
+        "/docente/responsables": function(){
+            importacionDinamica('responsables.js');
+        },
+        "/docente": function(){
+
+        }
+    };
+    try{
+        controllerMain[path]();
+    }catch(error){
+        console.error('Error: [JS001]' + error);
+    }
+    function importacionDinamica(url){
+        import('./../pages/docentes/' + url)
+            .then()
+    }
+})();
