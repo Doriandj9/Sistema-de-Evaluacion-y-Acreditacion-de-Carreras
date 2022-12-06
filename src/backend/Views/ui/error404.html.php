e81b067938d691ced13117bc107db366489bc3c8
DorianA <dorian9armijos@gmail.com>
2022-12-06 11:40:54 -0500

    feat: mejoro la presentacion de los errores

--- src/backend/Views/ui/error404.html.php
+++ src/backend/Views/ui/error404.html.php
@@ -1,3 +1,8 @@
-<h1>No se encontro es pagina</h1>
-
-<center><strong>404</strong></center>
+<div class="w-100 text-center">
+    <h1 style="font-size: 2rem;" class="text-center tipografia-new-1">No se encontro esta pagina</h1>
+    <img height="450"
+     src="/public/assets/img/undraw_page_not_found_re_e9o6.svg" alt="no encontrada">
+    <p class="tipografia-new-1">
+    <a href="/">Regresar</a>
+    </p>
+</div>
